005735bf54634a73a4f0f25b9d47408895e50d03
siva1982001 <72336823+siva1982001@users.noreply.github.com>
2020-10-26 11:35:13 +0530

    Update rvt.sh

--- rvt.sh
+++ rvt.sh
@@ -3,6 +3,6 @@ cd
 cd ..
 cd usr/etc
 rm bash.bashrc
-cd $HOME/TERMUX-WELCOME/Revert
+cd $HOME/termux-welcome-page/Revert
 mv bash.bashrc $PREFIC/etc
-python $HOME/TERMUX-WELCOME/Revert/Thanks.py
+python $HOME/termux-welcome-page/Revert/Thanks.py
